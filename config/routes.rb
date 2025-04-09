f9555d7210884e222cddade95c369e74e726edf1
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-04-09 20:01:12 +0000

    created user resources with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  root to: "home#index"
+  devise_for :users
+
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
